327043f3e65f59468bc82c72829b86be74fd51c9
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-08 22:30:46 +0300

    added create blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogCreateView
 
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='home'),
+    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from pytils.translit import slugify
 
 from blog.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'content', 'preview')
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
 
 
 class BlogListView(ListView):
