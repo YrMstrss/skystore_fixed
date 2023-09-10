cb20108eefba356288a35847778dcfe4c153255f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-10 13:47:37 +0300

    error correction

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from blog.apps import BlogConfig
 from blog.views import BlogListView, BlogCreateView
 
+app_name = BlogConfig.name
+
 urlpatterns = [
-    path('blog/', BlogListView.as_view(), name='home'),
+    path('blog/', BlogListView.as_view(), name='list'),
     path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
 ]
