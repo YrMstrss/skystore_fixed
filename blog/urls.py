8bc173df3ba1e97743b918390024a1396de281e1
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-10 15:11:53 +0300

    added url for edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogListView, BlogCreateView, BlogDetailView
+from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='list'),
     path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('blog/<str:slug>', BlogDetailView.as_view(), name='detail_blog')
+    path('blog/<str:slug>', BlogDetailView.as_view(), name='detail_blog'),
+    path('blog/edit/<str:slug>', BlogUpdateView.as_view(), name='edit_blog'),
 ]
