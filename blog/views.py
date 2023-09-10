c7f8e30b541f5f0276e32617ea37154ac70fde6e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-10 16:16:04 +0300

    added views counter

--- blog/views.py
+++ blog/views.py
@@ -27,6 +27,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+
+        return self.object
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
