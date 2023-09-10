65b8d579b01945a3e2a28ac3884f6a386ecef8d2
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-10 15:12:09 +0300

    added controller for edit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from pytils.translit import slugify
 
 from blog.models import Blog
@@ -27,3 +27,16 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
 
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'preview')
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
