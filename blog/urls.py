7322428d6b9769f5dee5cceb9f8c88ea15e415c3
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-10 15:45:12 +0300

    added url for delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView
+from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
     path('blog/<str:slug>', BlogDetailView.as_view(), name='detail_blog'),
     path('blog/edit/<str:slug>', BlogUpdateView.as_view(), name='edit_blog'),
+    path('blog/delete/<str:slug>', BlogDeleteView.as_view(), name='delete_blog'),
 ]
