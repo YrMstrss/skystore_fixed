9cf656af686b598865d6ca942bd30500762bdc5e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-18 21:12:56 +0300

    created controller for form

--- catalog/views.py
+++ catalog/views.py
@@ -1,33 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
-
-
-# def index_home(request):
-#     product = Product.objects.all()
-#     context = {
-#         'object_list': product
-#     }
-#     return render(request, "catalog/product_list.html", context)
-
-
-class ProductListView(ListView):
-    model = Product
 
 
 def index_contact(request):
     return render(request, "catalog/contact_page.html")
 
 
+class ProductListView(ListView):
+    model = Product
+
+
 class ProductDetailsView(DetailView):
     model = Product
 
 
-# def index_product(request, pk):
-#     product = Product.objects.get(pk=pk)
-#     context = {
-#         'object': product,
-#         'title': f'Товар - {product.name}'
-#     }
-#     return render(request, "catalog/product_detail.html", context)
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
