b8f887f26c598eff30d7061b95d7e9a338ee2786
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-18 21:13:04 +0300

    created url for form

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from catalog.views import index_contact, ProductListView, ProductDetailsView
+from catalog.apps import CatalogConfig
+from catalog.views import index_contact, ProductListView, ProductDetailsView, ProductCreateView
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', index_contact, name='contact'),
-    path('product/<int:pk>', ProductDetailsView.as_view(), name='product'),
+    path('product/<int:pk>', ProductDetailsView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
 ]
