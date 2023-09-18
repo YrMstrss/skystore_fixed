b083bde13be569a1ec49dba5ea5023ddc6431bb8
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-18 21:30:31 +0300

    name and description validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,30 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         exclude = ('creation_date', 'last_change_date')
+
+    def clean_name(self):
+        cleaned_name = self.cleaned_data.get('name')
+
+        prohibited_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                            'радар']
+
+        for word in prohibited_words:
+            if word in cleaned_name.lower():
+                raise forms.ValidationError('Запрещеночка в имени товара')
+
+        return cleaned_name
+
+    def clean_description(self):
+        cleaned_description = self.cleaned_data.get('description')
+
+        prohibited_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                            'радар']
+
+        for word in prohibited_words:
+            if word in cleaned_description.lower():
+                raise forms.ValidationError('Запрещеночка в описании товара')
+
+        return cleaned_description
