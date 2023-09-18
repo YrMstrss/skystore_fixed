930cd1f37d2f90025be7bed7abd22506c0465e4d
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-18 21:43:55 +0300

    added delete url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index_contact, ProductListView, ProductDetailsView, ProductCreateView, ProductUpdateView
+from catalog.views import index_contact, ProductListView, ProductDetailsView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('product/<int:pk>', ProductDetailsView.as_view(), name='product_detail'),
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('edit/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
 ]
