963a2c3d86e86a66350108a3bde27c19efeb4de6
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-21 21:28:13 +0300

    updated form_valid method

--- catalog/views.py
+++ catalog/views.py
@@ -40,6 +40,15 @@ class ProductUpdateView(UpdateView):
             context['formset'] = VersionFormset(instance=self.object)
         return context
 
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
+
 
 class ProductDeleteView(DeleteView):
     model = Product
