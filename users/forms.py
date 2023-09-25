3c603fe003fc563bbb1e3a6c53c4974903471ff5
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-25 22:17:09 +0300

    added style for profile and register form

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+from catalog.forms import StyleFormMixin
 from users.models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
 
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
 
-class UserProfileChangeForm(UserChangeForm):
+class UserProfileChangeForm(StyleFormMixin, UserChangeForm):
 
     class Meta:
         model = User
