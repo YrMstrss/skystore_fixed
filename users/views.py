9a5e78d0fd24e34b4c2551de550b092b1c503ed0
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-25 22:13:31 +0300

    added url and controller for profile page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileChangeForm
 from users.models import User
 
 
@@ -10,3 +10,13 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileChangeForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('catalog:home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
