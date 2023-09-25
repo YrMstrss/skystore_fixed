183c795e04ef40d127c835b4b76c66deabe9c0d8
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-25 21:54:25 +0300

    created profile form

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -8,4 +8,10 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
-        
+
+
+class UserProfileCageForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
