f03998ad1dd1d906d116488263f4735c170607c8
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-25 22:14:42 +0300

    hided field 'password'

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from users.models import User
@@ -15,3 +16,8 @@ class UserProfileChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widget = forms.HiddenInput()
