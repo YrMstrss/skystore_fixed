ce8450031a88837d483879940b05f2da38fa3b0b
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-26 21:03:32 +0300

    form_valid

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
+import secrets
+
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
+from config import settings
 from users.forms import UserRegisterForm, UserProfileChangeForm
 from users.models import User
 
@@ -10,6 +14,25 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_urlsafe(nbytes=8)
+
+        user.token = token
+        user.save()
+
+        url = reverse_lazy('users:verify', args=[token])
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Для подтверждения регистрации перейдите по ссылке: http://localhost:8000/{url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False
+        )
+
+        return super().form_valid(form)
 
 
 class ProfileView(UpdateView):
