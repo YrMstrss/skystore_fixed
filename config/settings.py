d724dafe6f240635e78e253b1ad710998b516d92
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-01 17:08:43 +0300

    added redis

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,11 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
